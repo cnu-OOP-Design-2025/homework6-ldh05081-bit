407cc32a3e951d3be9d839359594d8acb30c41f0
ldh05081-bit <ldh05081@gmail.com>
2025-11-03 15:12:42 +0900

    과제 제출

--- bird.cpp
+++ bird.cpp
@@ -1,3 +1,47 @@
 #include <iostream>
 #include "bird.h"
 
+class Duck : public Bird {
+public:
+    Duck() : Bird("Duck") {
+        cry();
+    }
+    void cry() override {
+        std::cout << "Quack!" << std::endl;
+    }
+    ~Duck() override {
+        cry();
+    }
+};
+
+class Penguin : public Bird {
+public:
+    Penguin() : Bird("Penguin") {
+        cry();
+    }
+    void cry() override {
+        std::cout << "Squawk!" << std::endl;
+    }
+    void fly() override {
+        std::cout << "This bird can't fly." << std::endl;
+    }
+    ~Penguin() override {
+        cry();
+    }
+};
+
+class Eagle : public Bird {
+public:
+    Eagle() : Bird("Eagle") {
+        cry();
+    }
+    void cry() override {
+        std::cout << "Screech!" << std::endl;
+    }
+    void swim() override {
+        std::cout << "This bird can't swim." << std::endl;
+    }
+    ~Eagle() override {
+        cry();
+    }
+};
